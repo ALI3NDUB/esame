fb9290ab3dc3d9b94871ace571568805f72304f1
ANDR3A(ALI3NDUB) <andrea.giorgio@email.it>
2023-05-16 11:15:38 +0200

    demo

--- app.py
+++ app.py
@@ -34,66 +34,46 @@ def main():
       
     add_bg_from_url()
  
-       
-    st.title('Company linear')
-    st.header('This app allows you to explore the Iris dataset and visualize the data using various plots.')
-    st.subheader("DataFrame")
+    st.title('Immobili')
+    st.header('Questa web-app sfrutta Streamlit per utilizzare un modello di machine learning')
+    st.subheader("DataFrame Originale")
     
-    path="https://frenzy86.s3.eu-west-2.amazonaws.com/python/data/Company.csv"
+    path="https://frenzy86.s3.eu-west-2.amazonaws.com/python/data/immobili.csv"
     df = pd.read_csv(path)
 
     st.dataframe(df)
 
-    X = df.drop(columns="Sales")
-    y = df["Sales"]
+    y = df["medv"]
+    X = df.drop(columns="medv")
 
     st.subheader('Input')
-    rd_spend = st.slider('TV', min_value=0, max_value=500, value=150)
-    admin = st.slider('Radio', min_value=0, max_value=500, value=150)
-    marketing = st.slider('Newspaper', min_value=0, max_value=500, value=150)
+    sl1 = st.slider('indus', min_value=0, max_value=500, value=150)
+    sl2 = st.slider('age', min_value=0, max_value=500, value=150)
+    sl3 = st.slider('tax', min_value=0, max_value=500, value=150)
 
-    input_data = {'TV': rd_spend,
-                  'Radio': admin,
-                  'Newspaper': marketing}
+    input_data = {'indus': sl1,
+                  'age': sl2,
+                  'tax': sl3}
 
     input_df = pd.DataFrame(input_data, index=[0])
-    prediction = new_model.predict([[rd_spend,admin,marketing]])
+    prediction = new_model.predict([[sl1,sl2,sl3]])
 
     st.subheader('Output')
-    st.write(f'Audience previsto: {prediction[0]}')
+    st.write(f'Predizione previsto: {prediction[0]}')
 
     # Plotly graph
-    fig = px.scatter_3d(df, x='TV', y='Radio', z='Newspaper', color_discrete_sequence=['red'])
+    fig = px.scatter_3d(df, x='lstat', y='rm', z='ptratio', color_discrete_sequence=['red'])
     fig.update_layout(
-        title="Regresione multipla",
+        title="Plot",
         scene=dict(
-            xaxis_title='TV',
-            yaxis_title='Radio',
-            zaxis_title='Newspaper'
+            xaxis_title='lstat',
+            yaxis_title='rm',
+            zaxis_title='ptradio'
         )
-    )
-    #fig.add_trace(px.scatter_3d(input_df, x='TV', y='Radio', z='Newspaper', color='Radio', opacity='[1]').data[0])
-    #st.plotly_chart(fig, use_container_width=True)
-    
-    #fig.add_trace(px.scatter_3d(input_df, x='TV', y='Radio', z='Newspaper', color_discrete_sequence=['Yellow'], size='Newspaper').data[0])  
-    #st.plotly_chart(fig, use_container_width=True)
-    
-    fig.add_trace(px.scatter_3d(input_df, x='TV', y='Radio', z='Newspaper', color='Newspaper', color_continuous_scale='oryel', opacity=0.5).data[0])
-    fig.update_layout(scene=dict(bgcolor='rgb(145, 160, 155)'))
-    st.plotly_chart(fig, use_container_width=True)
+    )  
 
-    predictions = new_model.predict(X)
-    custom_predictions = new_model.predict([[rd_spend, admin, marketing]])[0]
+    custom_predictions = new_model.predict([[sl1, sl2, sl3]])[0]
     st.write(custom_predictions)
-
-    if st.button('Scarica Excel'):
-        output = io.BytesIO()
-        writer = pd.ExcelWriter(output, engine='xlsxwriter')
-        input_df.to_excel(writer, index=False, sheet_name='Input')
-        pd.DataFrame(prediction, columns=['Profitto previsto']).to_excel(writer, index=False, sheet_name='Output')
-        writer.save()
-        output.seek(0)
-        st.download_button(label="Download", data=output, file_name='output.xlsx', mime='application/vnd.openxmlformats-officedocument.spreadsheetml.sheet',)
 
 if __name__ == '__main__':
     main()
